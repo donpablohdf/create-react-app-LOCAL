8e21991628c783b96ea5167b0aa7b221985dc8e3
donpablohdf <pabloalegrejuan@gmail.com>
2022-10-04 15:14:51 +0200

    Resuelto props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import PropType from "prop-types";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<BootstrapCard />);
 }
+
+const BootstrapCard = props => {
+  
+	// 1) replace the hard-coded image, description, link, etc. With their property variable.
+	return (
+		<div className="card m-5">
+			<img className="card-img-top" src={props.urlImg} />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonUrl} className="btn btn-primary">
+				{props.buttonLabel}
+				</a>
+			</div>
+		</div>
+	);
+};
+// 2) add here the new properties into the proptypes object
+BootstrapCard.propTypes = {
+	title: PropType.string,
+	urlImg: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string
+};
+BootstrapCard.defaultProps = {
+	title: "Bob Dylan",
+	urlImg: "https://upload.wikimedia.org/wikipedia/commons/0/02/Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg",
+	description: "Robert Dylan[3] (born Robert Allen Zimmerman; May 24, 1941) is an American singer-songwriter. Often regarded as one of the greatest songwriters of all time,[4][5][6] Dylan has been a major figure in popular culture during a career spanning more than 60 years",
+	buttonUrl: "https://en.wikipedia.org/wiki/Bob_Dylan",
+	buttonLabel: "Go to wikipedia"
+};
+
+
+
 
 export default App;
